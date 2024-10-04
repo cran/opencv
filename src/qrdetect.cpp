4a8bfa3a221dcb4166f27fd7ada7d2f07f3db12c
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-04 08:50:02 +0000

    version 0.4.1

--- src/qrdetect.cpp
+++ src/qrdetect.cpp
@@ -16,8 +16,12 @@ static std::string qr_scan_wechat(XPtrMat ptr, Mat &points){
 #endif
 
 static std::string qr_scan_opencv(XPtrMat ptr, Mat &points){
+#ifdef HAVE_QUIRC
   static QRCodeDetector qrDet = QRCodeDetector();
   return qrDet.detectAndDecode(get_mat(ptr), points);
+#else
+  throw std::runtime_error("QR scanner requires OpenCV 3.4.4 or newer");
+#endif
 }
 
 // [[Rcpp::export]]
@@ -30,12 +34,22 @@ SEXP cvmat_qr_detect(XPtrMat ptr, bool return_image = false, bool use_wechat = t
 #endif
   if(!data.length())
     return R_NilValue;
-  if(!return_image)
-    return Rcpp::CharacterVector::create(data);
-  cv::Mat img = get_mat(ptr);
   if(!use_wechat)
     points = points.reshape(1, 4);
-  for (int i = 0; i < points.size().height; i++) {
+  int len = points.size().height;
+  Rcpp::IntegerMatrix corners(len, 2);
+  for (int i = 0; i < len; i++) {
+    Point pt = cv::Point(points.row(i));
+    corners[i] = pt.x;
+    corners[i+len] = pt.y;
+  }
+  if(!return_image){
+    Rcpp::CharacterVector out(data);
+    out.attr("points") = corners;
+    return out;
+  }
+  cv::Mat img = get_mat(ptr);
+  for (int i = 0; i < len; i++) {
     Point pt1 = cv::Point(points.row(i));
     Point pt2 = cv::Point(points.row((i + 1) % 4));
     line(img, pt1, pt2, Scalar(255, 0, 0), 3);
@@ -47,6 +61,7 @@ SEXP cvmat_qr_detect(XPtrMat ptr, bool return_image = false, bool use_wechat = t
               CV_RGB(118, 185, 0), //font color
               2);
   ptr.attr("value") = Rcpp::CharacterVector::create(data);
+  ptr.attr("points") = corners;
   return ptr;
 }
 
